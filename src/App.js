1c403de9de036b02569149bd062fd5f08151d2e9
Ivanilton Bezerra <nitinhosilva1996@gmail.com>
2020-12-16 22:15:05 -0300

    adding hearder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import "./App.css";
+
+import Home from "./Components/Home";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Login from "./Components/Login/Login";
 
 const App = () => {
-  return <div>App React</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
