a59c9854cc6e2fa877b4c9b33469deb9b0b60cd6
Ivanilton Bezerra <nitinhosilva1996@gmail.com>
2021-01-13 23:16:27 -0300

    feed Part 1

---
+++ src/Components/Feed/FeedPhotos.js
@@ -0,0 +1,38 @@
+import React from "react";
+import FeedPhotosItem from "./FeedPhotosItem";
+import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
+import { PHOTOS_GET } from "../../api";
+import styles from "./FeedPhotos.module.css";
+
+const FeedPhotos = () => {
+  const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    async function fetchPhotos() {
+      const { url, option } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+      const { json } = await request(url, option);
+
+      console.log("json", json);
+    }
+
+    fetchPhotos();
+  }, [request]);
+
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (data) {
+    return (
+      <ul className={`${styles.feed} animeLeft`}>
+        {data.map((photo) => (
+          <FeedPhotosItem key={photo.id} photo={photo} />
+        ))}
+      </ul>
+    );
+  } else {
+    return null;
+  }
+};
+
+export default FeedPhotos;
